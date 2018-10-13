575c9a8e24ab5a7d07ebaae4ca3047f64e4ae2b9
DrJessop <33402477+DrJessop@users.noreply.github.com>
2018-10-13 16:24:05 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,14 +2,43 @@ package frontend;
 
 public class Main {
 	
+	public static char login(Scanner scanner) {
+		boolean loggedIn = false;
+		String input;
+		char userState = '\0';
+		while (!loggedIn) {
+			System.out.println("Please enter agent or planner: ");
+			input = scanner.nextLine();
+			if (input.equals("agent")) {
+				userState = 'a';
+				loggedIn = true;
+			}
+			else if (input.equals("planner")) {
+				userState = 'p';
+				loggedIn = true;
+			}
+			else 
+				System.out.println("Invalid input");
+		}
+		return userState;
+	}
+	
 	public static void main(String[] args) {
 		
+		ArrayList<String> transactionMessages = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
+		String fileName = "transactionSummaryFile.txt";
+		User user;
 		while (true) {
-			String fileName = "transactionSummaryFile.txt";
 			
-			/*
-			 * Deals with Logging-in
-			 */
+			char userState = login(scanner);
+			
+			if (userState == 'a') 
+				user = (Agent) new Agent(fileName);
+			else
+				user = (Planner) new Planner(fileName);
+			
+			
 		}
 		
 	}
