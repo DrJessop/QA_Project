0d50853453684772736aafef16cce82c64db0e0c
DrJessop <33402477+DrJessop@users.noreply.github.com>
2018-10-30 20:50:53 -0400

    Add files via upload

--- Test Files/cancelticket_inputs/testCancel.sh
+++ Test Files/cancelticket_inputs/testCancel.sh
@@ -1,5 +1,5 @@
 for i in *.txt
 do
-	java -jar ../frontend/frontend.jar ../validservices.txt ../cancelticket_program_outputs/transaction_summary_files/${i%.*}_tsf.txt 2> /dev/null < $i > \
-	../cancelticket_program_outputs/terminal_outputs/${i%.*}.log 
+	java -jar ../frontend/frontend.jar ../validservices.txt ../cancelticket_program_outputs/transaction_summary_files/${i%.*}_tsf.log 2> /dev/null < $i > \
+	../cancelticket_program_outputs/terminal_outputs/${i%.*}_to.log 
 done
