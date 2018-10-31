3109942eea0d2a1b6c634d7a4af137e8370caa17
DrJessop <33402477+DrJessop@users.noreply.github.com>
2018-10-30 20:53:36 -0400

    Add files via upload

--- Test Files/logout_inputs/testLogout.sh
+++ Test Files/logout_inputs/testLogout.sh
@@ -1,5 +1,5 @@
 for i in *.txt
 do
-	java -jar ../frontend/frontend.jar ../validservices.txt ../logout_program_outputs/transaction_summary_files/${i%.*}_tsf.txt 2> /dev/null < $i > \
-	../logout_program_outputs/terminal_outputs/${i%.*}.log 
+	java -jar ../frontend/frontend.jar ../validservices.txt ../logout_program_outputs/transaction_summary_files/${i%.*}_tsf.log 2> /dev/null < $i > \
+	../logout_program_outputs/terminal_outputs/${i%.*}_to.log 
 done
